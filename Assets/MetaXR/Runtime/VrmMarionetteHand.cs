490a69b41fc8c8036e182fc2ec33139dca98d2a3
nosix <nosix@users.noreply.github.com>
2023-12-25 18:57:16 +0900

    コントローラー対応を追加する

--- Assets/MetaXR/Runtime/VrmMarionetteHand.cs
+++ Assets/MetaXR/Runtime/VrmMarionetteHand.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
@@ -10,7 +9,8 @@ namespace VRMarionette.MetaXR
     {
         public float grabThresholdDistance = 0.04f;
 
-        public OVRSkeleton skeleton;
+        public OVRHand hand;
+        public OVRControllerHelper controller;
 
         [Header("Spring Bone Colliders")]
         public VRM10SpringBoneCollider palm;
@@ -20,6 +20,10 @@ namespace VRMarionette.MetaXR
         public VRM10SpringBoneCollider ring;
 
         public UnityEvent<bool> onGrab;
+
+        private OVRSkeleton _skeleton;
+
+        private Transform _srcControllerTransform;
 
         private Transform _srcPalmTransform;
         private Transform _srcThumbTransform;
@@ -31,14 +35,28 @@ namespace VRMarionette.MetaXR
         private Transform _dstIndexTransform;
         private Transform _dstRingTransform;
 
+        private bool _isHandTracking;
         private bool _isGrabbing;
 
-        // Start is called before the first frame update
         private IEnumerator Start()
         {
-            while (!skeleton.IsInitialized) yield return null; // 1フレーム待つ
+            _dstPalmTransform = palm.transform;
+            _dstThumbTransform = thumb.transform;
+            _dstIndexTransform = index.transform;
+            _dstRingTransform = ring.transform;
 
-            foreach (var bone in skeleton.Bones)
+            if (controller is not null)
+            {
+                _srcControllerTransform = controller.transform;
+            }
+
+            if (hand is null) yield break;
+            _skeleton = hand.GetComponent<OVRSkeleton>();
+            if (_skeleton is null) yield break;
+
+            while (!_skeleton.IsInitialized) yield return null; // 1フレーム待つ
+
+            foreach (var bone in _skeleton.Bones)
             {
                 switch (bone.Id)
                 {
@@ -56,21 +74,55 @@ namespace VRMarionette.MetaXR
                         break;
                 }
             }
-
-            _dstPalmTransform = palm.transform;
-            _dstThumbTransform = thumb.transform;
-            _dstIndexTransform = index.transform;
-            _dstRingTransform = ring.transform;
         }
 
-        // Update is called once per frame
         private void Update()
         {
-            if (!skeleton.IsDataValid) return;
+            // Controller に切り替える
+            if (_isHandTracking && !hand.IsTracked)
+            {
+                _isHandTracking = false;
+                _dstThumbTransform.gameObject.SetActive(false);
+                _dstIndexTransform.gameObject.SetActive(false);
+                _dstRingTransform.gameObject.SetActive(false);
+            }
+
+            // Hand Tracking に切り替える
+            if (!_isHandTracking && hand.IsTracked)
+            {
+                _isHandTracking = true;
+                _dstThumbTransform.gameObject.SetActive(true);
+                _dstIndexTransform.gameObject.SetActive(true);
+                _dstRingTransform.gameObject.SetActive(true);
+            }
+
+            if (_isHandTracking) SyncSkeleton();
+            else SyncController();
+        }
+
+        private void SyncController()
+        {
+            if (controller is null) return;
+
+            _dstPalmTransform.position = _srcControllerTransform.position;
+            _dstPalmTransform.rotation = _srcControllerTransform.rotation;
+
+            var yAngle = controller.m_controller switch
+            {
+                OVRInput.Controller.LTouch or OVRInput.Controller.LHand => 90f,
+                OVRInput.Controller.RTouch or OVRInput.Controller.RHand => -90f,
+                _ => 0f
+            };
+            _dstPalmTransform.Rotate(0f, yAngle, 0f);
+        }
+
+        private void SyncSkeleton()
+        {
+            if (_skeleton is null || !_skeleton.IsDataValid) return;
 
             // 手の位置と向きを同期する
             var dstHandTransform = transform;
-            var srcHandTransform = skeleton.transform;
+            var srcHandTransform = _skeleton.transform;
             dstHandTransform.position = srcHandTransform.position;
             dstHandTransform.rotation = srcHandTransform.rotation;
 
@@ -79,13 +131,11 @@ namespace VRMarionette.MetaXR
             _dstPalmTransform.position = _srcPalmTransform.position;
             _dstPalmTransform.rotation = _srcPalmTransform.rotation;
 
-            var xAngle = skeleton.GetSkeletonType() switch
+            var xAngle = _skeleton.GetSkeletonType() switch
             {
                 OVRSkeleton.SkeletonType.HandLeft => -90f,
                 OVRSkeleton.SkeletonType.HandRight => 90f,
-                OVRSkeleton.SkeletonType.Body => 0f,
-                OVRSkeleton.SkeletonType.None => 0f,
-                _ => throw new ArgumentOutOfRangeException()
+                _ => 0f
             };
             _dstPalmTransform.Rotate(xAngle, 0f, 0f);
 
@@ -108,6 +158,11 @@ namespace VRMarionette.MetaXR
             {
                 onGrab.Invoke(_isGrabbing);
             }
+        }
+
+        public void Grab(bool on)
+        {
+            onGrab.Invoke(on);
         }
     }
 }
